87931ee195d39673e683b4c97fa1da91734a91aa
CodaarX <michaelisesele@gmail.com>
2020-11-08 11:28:00 +0100

    mall bot updated
    mall bot updated

--- js/main.js
+++ js/main.js
@@ -1,20 +1,45 @@
-var name 
-var age 
-var maskChecker = prompt("do you have a facemask?")
+
+var name = document.getElementById("name")
+var age = document.getElementById("age")
+var maskChecker = document.getElementById("select")
 
 var totalVisitors = 0
-
 var deniedVisitors = 0
-
 var allowedVisitors = 0
 
+function start(){
+if(name.value !== "string"){
+        alert("Input your name")
+    } else if(age < 0 || age > 100 ){
+        alert("please input a valid age")
+    } else if(age < 12) {
+        alert("Sorry, you have to be 12 years or older to enter Eminence mall")
+        deniedVisitors++
+    } else if(maskChecker !== "yes"){
+        alert("Sorry, you need to have your facemask on to enter")
+        deniedVisitors++
+    } 
+    
+    else {alert('welcome to Eminence Mall, ' + name)}
+        allowedVisitors++
 
-function startChecker(){
-    name = prompt("Input your name")
-    if (name === null) {
-        startChecker
-    }   age = prompt("input your age")
-            if(age === null){
-            age = prompt ("inut your age")
-    }           
+totalVisitors++
+
+
+// counter
+totalVisitors = document.getElementById("totalV").textContent = `Total visitors: ${totalVisitors++}`
+
+deniedVisitors = document.getElementById("deniedV").textContent = `Denied visitors: ${deniedVisitors++}`
+
+allowedVisitors = document.getElementById("allowedV").textContent = `Allowed visitors: ${allowedVisitors++}`
+
+name.value = ''      
+age.value = ''
+maskChecker.value = 'No'
+
 }
+
+
+var button = document.getElementById("btn").onclick(start())
+
+
